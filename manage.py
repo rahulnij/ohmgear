3fb65fdb548b227e6fb507296d6fec41c4faa89a
sazid <sazidk@clavax.us>
2015-08-03 11:22:07 +0530

    Set up

--- manage.py
+++ manage.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 import os
 import sys
+import socket
+import re
 
+#----------- clavax:Set setting according server -------------------------#
+HOSTNAME = socket.gethostname().lower().split('.')[0].replace('-','')
+if re.search('clavax',  HOSTNAME):
+   settings = "ohmgear.settings.local"
+else:
+   settings = "ohmgear.settings.server"
+#----------- End Setting -------------------------------------------#
+   
+   
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ohmgear.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
     from django.core.management import execute_from_command_line
 
--- ohmgear/urls.py
+++ ohmgear/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework.authtoken import views
+import views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'ohmgear.views.home', name='home'),
     #url(r'^blog/', include('blog.urls')),
     url(r'^api/', include('apps.users.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-token-auth/', views.obtain_auth_token)
+    url(r'^token/', views.obtain_expiring_auth_token)
 ]
