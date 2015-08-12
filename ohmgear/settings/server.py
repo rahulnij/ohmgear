855edb6554b39d80d2bca0d84dd74bd98b161d50
sazid <sazidk@clavax.us>
2015-08-12 15:21:48 +0530

    OHM-7

--- ohmgear/settings/server.py
+++ ohmgear/settings/server.py
@@ -2,7 +2,7 @@ from base import *
 DEBUG = False
 SITE_ROOT="/var/www/websites/ohmgear/"
 STATIC_ROOT = "/var/www/websites/ohmgear/ohmgear/static/"
-ALLOWED_HOSTS = ['192.168.2.129']
+#ALLOWED_HOSTS = ['192.168.2.129']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
