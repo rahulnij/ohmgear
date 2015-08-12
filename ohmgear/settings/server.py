f0a1025a5078ee35add865f51b0ed22220f41ce9
sazid <sazidk@clavax.us>
2015-08-12 14:26:24 +0530

    OHM-7

--- ohmgear/settings/server.py
+++ ohmgear/settings/server.py
@@ -1,8 +1,7 @@
-from ohmgear.settings.base import *
+from base import *
 DEBUG = False
 SITE_ROOT="/var/www/websites/ohmgear/"
 STATIC_ROOT = "/var/www/websites/ohmgear/ohmgear/static/"
-SECRET_KEY = '3-6ostd%53*04ccinr@c0^y!)^op6ue@ekd-cka86^90+z=jln'
 ALLOWED_HOSTS = ['192.168.2.129']
 DATABASES = {
     'default': {
