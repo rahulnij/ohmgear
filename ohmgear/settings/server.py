a7c357f4e063952cfff3b97db662262098003d32
sazid <sazidk@clavax.us>
2015-08-12 13:57:05 +0530

    OHM-7

--- ohmgear/settings/server.py
+++ ohmgear/settings/server.py
@@ -2,7 +2,7 @@ from ohmgear.settings.base import *
 DEBUG = False
 SITE_ROOT="/var/www/websites/ohmgear/"
 STATIC_ROOT = "/var/www/websites/ohmgear/ohmgear/static/"
-
+SECRET_KEY = '3-6ostd%53*04ccinr@c0^y!)^op6ue@ekd-cka86^90+z=jln'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
