15ab8c81817d100e6e93437b0c0b3d7585604f3e
agumber <agumber@clavax.us>
2015-08-19 16:23:25 +0530

    make a new module for Identifiers and module for it.

--- apps/identifiers/models.py
+++ apps/identifiers/models.py
@@ -13,17 +13,20 @@ TEMPLATE_TYPE = (('1', 'option1'),
                   ('2', 'option2'),
                   ('3', 'option3'),                  
                  )
-class BusinessCard(models.Model):
+      
+IDENTIFIER_TYPE = (('1','System Generated'),
+                    ('2','Premium'),
+                    )
+class Identifier(models.Model):
     
     class Meta:
-     db_table = 'ohmgear_businesscard'
-    name = models.CharField(_("name"),null=True,max_length=50)
-    template_id = models.IntegerField(_("Template Id"),choices=TEMPLATE_TYPE,default=1)
-    status = models.IntegerField(_("Status"),default=0)
-    is_Actve = models.IntegerField(_("Is Active"),default=1)
+     db_table = 'ohmgear_identifier'
+    user = models.OneToOneField(User)
+    identifier = models.CharField(_("identifier"),null=True,max_length=50)
+    identifier_type = models.IntegerField(_("Identifier Type"),choices=IDENTIFIER_TYPE,default=1)
     created_date = models.DateTimeField(_("Created Date"),auto_now_add=True)
     updated_date = models.DateTimeField(_("Updated Date"),auto_now_add= True)
-    user = models.OneToOneField(User)
+    
     
     def __unicode__(self):
-        return'{"id:"%s","name":"%s","template_id":"%s"}'%(self.id,self.template_id)
+        return'{"id:"%s","identifier":"%s","identifier_type":"%s"}'%(self.id,self.identifier,self.identifier_type)
