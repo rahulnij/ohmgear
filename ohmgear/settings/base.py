5f27c6bcd44e08d22ca909ec188740fe0fc4b78c
agumber <agumber@clavax.us>
2015-08-19 15:18:25 +0530

    Make a new module for Business card and make a model for it.

--- ohmgear/settings/base.py
+++ ohmgear/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.users',
+    'apps.managebusinesscards',
     'rest_framework',
     'rest_framework.authtoken',
 )
