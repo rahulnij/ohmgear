baaf10493222d3d7c418e1e8a387d70c9d5e2f29
agumber <agumber@clavax.us>
2015-08-25 11:48:42 +0530

    New model for template of business card.

--- apps/businesscards/models.py
+++ apps/businesscards/models.py
@@ -5,16 +5,27 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 # Create your models here.
 
-TEMPLATE_TYPE = (('1', 'option1'),
-                  ('2', 'option2'),
-                  ('3', 'option3'),                  
-                 )
+class BusinessCardTemplate(models.Model):
+    
+        class Meta:
+            db_table = 'ohmgear_businesscardtemplate'
+        template_name = models.CharField(_("Template Name"),max_length=50)
+        template_content = models.CharField(_("Template Content"),max_length= 100)
+        status  = models.IntegerField(_("Status"),default=0)
+        created_date = models.DateTimeField(_('Created Date'))
+        updated_date = models.DateTimeField(_('Updated Date'))
+        
+        def __unicode__(self):
+            return '{"id":"%s","template_name":"%s","template_content":"%s"}' %(self.id,self.template_name,self.template_content)
+    
+
+
 class BusinessCard(models.Model):
     
     class Meta:
      db_table = 'ohmgear_businesscard'
     name = models.CharField(_("name"),null=True,max_length=50)
-    template_id = models.IntegerField(_("Template Id"),choices=TEMPLATE_TYPE,default=1)
+    template = models.OneToOneField('BusinessCardTemplate')
     status = models.IntegerField(_("Status"),default=0)
     is_Actve = models.IntegerField(_("Is Active"),default=1)
     created_date = models.DateTimeField(_("Created Date"),auto_now_add=True)
@@ -36,13 +47,9 @@ class BusinessCardIdentifier(models.Model):
     created_date    =  models.DateTimeField(_('Created Date'),auto_now_add = True)
     updated_date    =   models.DateTimeField(_('Update Date'),auto_now_add = True)
     
+    def __unicode__(self):
+        return'{"id:"%s","businesscard":"%s","identifier":"%s"}'%(self.id,self.businesscard,self.identifier)
     
     
-    class BusinessCardTemplate(models.Model):
-        db_table = 'ohmgear_businesscardtemplate'
-        templateName = models.CharField(_("Template Name"),null=True,max_length=50)
-        templateContent = models.CharField(_("Template Content"),max_length= 100)
-        status  = models.IntegerField(_("Status"),default=0)
-        created_date = models.DateTimeField(_('Created Date'))
-        updated_date = modelsDateTimeField(_('Updated Date'))
+    
     
