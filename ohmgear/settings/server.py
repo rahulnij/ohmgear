d9ef7be4302b4baaeff218c2c6bdaf48cf1a022b
sazid <sazidk@clavax.us>
2015-09-09 17:01:33 +0530

    OHM

--- ohmgear/settings/server.py
+++ ohmgear/settings/server.py
@@ -1,5 +1,6 @@
 from base import *
-DOMAIN_NAME = 'http://ohmgear.in'
+#DOMAIN_NAME = 'http://ohmgear.in'http://192.168.2.129:8018
+DOMAIN_NAME = 'http://192.168.2.129:8018'
 DEBUG = True
 SITE_ROOT="/var/www/websites/ohmgear/"
 STATIC_ROOT = "/var/www/websites/ohmgear/ohmgear/static/"
