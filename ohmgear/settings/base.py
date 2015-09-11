38b6308a2139d68a798e08b35c0c28a94436582d
sazid <sazidk@clavax.us>
2015-09-11 11:02:28 +0530

    OHM

--- ohmgear/settings/base.py
+++ ohmgear/settings/base.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3-6ostd%53*04ccinr@c0^y!)^op6ue@ekd-cka86^90+z=jln'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
