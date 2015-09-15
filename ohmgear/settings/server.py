a9f247f131f3485cb887856a47eca6e911ed49fe
sazid <sazidk@clavax.us>
2015-09-15 18:27:14 +0530

    OHM

--- ohmgear/settings/server.py
+++ ohmgear/settings/server.py
@@ -31,4 +31,4 @@ CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_IMPORTS=("BaseSendMail")
+CELERY_IMPORTS=("apps.email.views.BaseSendMail")
