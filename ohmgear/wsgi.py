93b715ee75fd3ef52bff80784f1442609cb5a3f5
sazid <sazidk@clavax.us>
2015-09-18 16:24:34 +0530

    resend mail

--- ohmgear/wsgi.py
+++ ohmgear/wsgi.py
@@ -8,6 +8,16 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import socket
+import re
+#----------- clavax:Set setting according server -------------------------#
+HOSTNAME = socket.gethostname().lower().split('.')[0].replace('-','')
+if re.search('clavax',  HOSTNAME):
+   settings = "ohmgear.settings.local"
+else:
+   settings = "ohmgear.settings.server"
+#----------- End Setting -------------------------------------------#
 
 from django.core.wsgi import get_wsgi_application
 
