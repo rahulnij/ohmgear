d9790fa5cc094623dc5ad2e3fae8ca03ba059db4
sazid <sazidk@clavax.us>
2015-09-18 16:26:22 +0530

    resend mail

--- ohmgear/wsgi.py
+++ ohmgear/wsgi.py
@@ -21,6 +21,6 @@ else:
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ohmgear.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
 application = get_wsgi_application()
