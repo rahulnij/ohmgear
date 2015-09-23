98ba00e209ba19856b51122269a51ae5e12f986d
agumber <agumber@clavax.us>
2015-09-23 17:21:24 +0530

    changes in email validation message.

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -2,9 +2,11 @@ from django.conf.urls import url, include
 from apps.users.models import User,Profile,SocialLogin
 from rest_framework import routers, serializers, viewsets
 from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
 # Serializers define the API representation.
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True,required=False,allow_blank=True)
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=get_user_model().objects.all(),message='Email id is already registered with ohmgear')])
     class Meta:
         model = get_user_model()
         fields = ('id','account_number','first_name','last_name','email','email_verification_code','user_type','pin_number','status','password')
@@ -13,10 +15,9 @@ class UserSerializer(serializers.ModelSerializer):
     def validate_user_type(self, attrs):
         value = attrs
         if value.id == 1:
-            print '1'
             raise serializers.ValidationError("user_type must be individual")
         elif value.id == 2:
-            print "2"
+            pass
         else: 
             print "other"
             raise serializers.ValidationError("user_type must be individual")
