e0db77ac96f027e1649d6b9071620d6fb53a2ed7
sazid <sazidk@clavax.us>
2015-10-15 14:38:34 +0530

    CHang

--- apps/users/views.py
+++ apps/users/views.py
@@ -299,6 +299,7 @@ def useractivity(request,**kwargs):
         elif op == 'reset_password':
             
                 email = request.POST.get('email','')
+                profile = Profile.objects.select_related().get(user__email=request.DATA['email'],user__user_type__in=[2,3])
                 try:
                  profile = Profile.objects.select_related().get(user__email=request.DATA['email'],user__user_type__in=[2,3])
                 except:
