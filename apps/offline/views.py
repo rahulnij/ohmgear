f8fd52feaf9d0854e8c8f722022065c7997717a8
agumber <agumber@clavax.us>
2016-02-01 18:39:26 +0530

    update muliple usersetting while offline

--- apps/offline/views.py
+++ apps/offline/views.py
@@ -13,6 +13,8 @@ import rest_framework.status as status
 #------------ Local app imports ------#
 from apps.businesscards.views import BusinessViewSet
 from apps.businesscards.models import BusinessCard
+from apps.usersetting.models import Setting,UserSetting
+from apps.usersetting.serializer import UserSignupSettingSerializer
 from ohmgear.functions import CustomeResponse
 from ohmgear.token_authentication import ExpiringTokenAuthentication
 #-------------------------------------------#
@@ -64,6 +66,67 @@ class OfflineSendReceiveDataViewSet(viewsets.ModelViewSet):
       @list_route(methods=['get'],)
       def receive(self, request):
           pass      
+
+
+      @list_route(methods=["post"],)
+      # update multiple settings in case of offline----------#
+      def updatemultiplerecord(self,request):
+        try:
+            getkey  =request.DATA
+
+            settingdata = Setting.objects.all()
+            usersettingdata = UserSetting.objects.all()
+            
+            usersettingexist = UserSetting.objects.filter(user_id=request.user.id)
+            if usersettingexist:
+            
+           # get corresponding key and setting id from setting table-----------#
+                tempContainer = []
+                count = 0
+                for i in getkey: 
+                    innerloop= 0
+                    for j in settingdata:
+                        data   = {}
+                        if i==j.key:
+                            data = {"key":j.key,"setting_id":j.id}
+                            datas = tempContainer.append(data)                        
+                            count=count+1
+            
+            # make dictionary for setting_id corresponding to key and its value to be update in usersetting table-------#
+            
+                newtempContainer = []
+                counter=0
+                for getdata in getkey:
+                    existingdata    =   0   
+                    for i in tempContainer:
+                        data    =   {}
+                        if getdata in i['key']:
+                            value   =  getkey[i['key']]
+                            data    =   {"setting_id":i['setting_id'],"value":value,"user_id":request.user.id}
+                            datas = newtempContainer.append(data)                        
+                            counter=counter+1
+
+            # update usersetting records corresponding to setting_id in usersetting table--------#
+                    j=0         
+                    for i in usersettingdata:
+                        for k in newtempContainer :
+                            if i.setting_id.id == k['setting_id'] and k['user_id']==i.user_id.id:
+                                UserSetting.objects.filter(user_id=request.user.id,setting_id=k['setting_id']).update(value=k['value'])
+                                j=j+1
+                                
+                if newtempContainer:
+                    return CustomeResponse({"msg":"Data has been updated"},status=status.HTTP_200_OK)
+                else:
+                    return CustomeResponse({"msg":" No Data found"},status=status.HTTP_400_BAD_REQUEST,validate_errors=1)
+                        
+            else:
+                return CustomeResponse({"msg":" No Data found for this user"},status=status.HTTP_400_BAD_REQUEST,validate_errors=1)
+                                                                                     
+        except:
+            return CustomeResponse({"msg":"Data not found"},validate_errors=1)
+          
+
+
 
 #---------------------------- End ----------------------------------------------#
 
