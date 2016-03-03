1a8a7603cf8b30be9b4cc2c1fc13afed9003fe93
sazid <sazidk@clavax.us>
2016-03-03 18:28:31 +0530

    Changes in jsong

--- ohmgear/json_default_data.py
+++ ohmgear/json_default_data.py
@@ -115,9 +115,9 @@ BUSINESS_CARD_DATA_VALIDATION = {
                                 "items": {
                                           "type" : "object",
                                           "properties" : {
-                                           "indexPos": { "type": "number" },
+                                           "indexPos": { "type": "integer" },
 		                           "placeHolder": { "type" : "string" },
-                                           "isUpper":{ "type": "string" },
+                                           "isUpper":{ "type": "integer" },
                                            "keyName":{ "type" : "string","enum": ["FirstName","LastName","CompName","CardName","DEPTName","NickName"] },
                                            "value":{ "type" : "string"}    
                                           }
