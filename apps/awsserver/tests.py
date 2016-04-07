851d968369e075f476a9bb8e1f01598daf2b0193
sazid <sazidk@clavax.us>
2016-04-07 17:50:58 +0530

    test cases

--- apps/awsserver/tests.py
+++ apps/awsserver/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase,APIClient
+from apps.users.functions import getToken
+from apps.users.models import User
+# Test api : create business card
 
-# Create your tests here.
+
+#class BusinessCardTests(APITestCase):
+#  client = APIClient()
+#
+#  fixtures = ['default']
+#  
+#  def setUp(self):
+#    url = '/api/users/'
+#    data = {"first_name":"sazid","email":"test@kinbow.com","password":"1111","user_type":2,"status":1}
+#    response = self.client.post(url, data, format='json')
+#    
+#  def test_register_to_aws(self):
+#    user = User.objects.get(email='test@kinbow.com')
+#    Token = getToken(user.id)
+#    #----------- insert template -------------------#
+#    
+#    #-----------------------------------------------#
+#    data = {"bcard_json_data":'{"side_first": {"basic_info": [{"value": "MyBusinesscar", "isUpper": "1", "keyName": "CardName", "indexPos": "0", "placeHolder": "NAME THIS CARD (Required)"}, {"value": "Detail Check", "isUpper": "1", "keyName": "FirstName", "indexPos": "2", "placeHolder": "First Name(Required)"}, {"value": "sdfsdfsdfdsf", "isUpper": "1", "keyName": "LastName", "indexPos": "3", "placeHolder": "Last Name"}, {"value": "sdfsdf", "isUpper": "1", "keyName": "NickName", "indexPos": "4", "placeHolder": "Nick Name Or Alias"}, {"value": "asfasf", "isUpper": "1", "keyName": "DEPTName", "indexPos": "5", "placeHolder": "Title & Department"}, {"value": "zvzxvx", "isUpper": "1", "keyName": "CompName", "indexPos": "6", "placeHolder": "Company Name"}], "contact_info": {"email": [{"data": "hhhhh@fff.fff1", "type": "home"}, {"data": "sdfsdf1@asfasfa.khk", "type": "work"}, {"data": "asdfas1@wee.qeq", "type": "iCloud"}], "phone": [{"data": "(122) 222-221", "type": "home", "countryCode": "+93", "countryFlag": "AF"}, {"data": "(yyy) yyy-yyy1", "type": "work", "countryCode": "+93", "countryFlag": "AF"}]}}, "side_second": {"contact_info": {"email": [{"data": "sec_12341@asf.dfsas", "type": "home"}, {"data": "sec_12351@adds.fghf", "type": "work"}, {"data": "asdfas1@gaff.utyu", "type": "iCloud"}], "phone": [{"data": "sec_12341", "type": "home", "countryCode": "+93", "countryFlag": "AF"}, {"data": "1234123412341", "type": "home", "countryCode": "+93", "countryFlag": "AF"}]}}}'}
+#    auth_headers = {
+#    'HTTP_AUTHORIZATION': 'Token '+str(Token),
+#    }
+#    response = self.client.post('/api/businesscard/',data,format='json',**auth_headers)
+#    self.assertEqual(response.status_code, 201)
+ 
--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,47 +1,42 @@
-from rest_framework import status
-from rest_framework.test import APITestCase,APIClient
-from ohmgear.functions import sql_select
-from apps.users.models import User,UserType
-from apps.email.models import EmailTemplate
-from functions import getToken
-## Test api : user registration
+from rest_framework.test import APITestCase, APIClient
+# Test api : user registration
 #           : user login
 #           : social login
-#           : forgot passoword
+
 
 class UserTests(APITestCase):
-  client = APIClient()
+    client = APIClient()
 
-  fixtures = ['default']
-  def setUp(self):
-      pass
-    
-  def test_user_registration(self):
-    url = '/api/users/'
-    data = {"first_name":"sazid","email":"sazid.se@gmail.com","password":"1111","user_type":2,"status":1}
-    response = self.client.post(url, data, format='json')
-    self.assertEqual(response.status_code, 201)
-    
-  def test_user_login(self):
+    fixtures = ['default']
 
-    url = '/api/useractivity/'
-    data = {"op":"login","username":"sazid.se@gmail.com","password":"1111"}
-    response = self.client.post(url, data)
-    self.assertEqual(response.status_code, 200) 
-     
-  def test_social_login(self):
+    def setUp(self):
+        url = '/api/users/'
+        data = {
+            "first_name": "sazid",
+            "email": "test@kinbow.com",
+            "password": "1111",
+            "user_type": 2,
+            "status": 1}
+        response = self.client.post(url, data, format='json')
 
-    url = '/api/sociallogin/'
-    data = {"first_name":"sazid","email":"sazid.se1@gmail.com","status":1,"user_type":2}
-    response = self.client.post(url, data, format='json')
-    self.assertEqual(response.status_code, 201)
-    
-#  def test_profile(self):
-#    user = User.objects.get(email='sazidk@clavax.us')
-#    Token = getToken(user.id)
-#    auth_headers = {
-#    'Authorization': 'Token'+str(Token),
-#    }
-#    response = self.client.post('/api/profile/', **auth_headers)
-#    
-#    print response
+    def test_user_login(self):
+
+        url = '/api/useractivity/'
+        data = {
+            "op": "login",
+            "username": "test@kinbow.com",
+            "password": "1111"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_social_login(self):
+
+        url = '/api/sociallogin/'
+        data = {
+            "first_name": "sazid",
+            "email": "test1@kinbow.com",
+            "status": 1,
+            "user_type": 2,
+            "social_type": "FB"}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 201)
