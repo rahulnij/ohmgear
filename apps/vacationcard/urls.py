498566d7295767ea2f519b3f39f946b98a5302a4
dsharma <dsharma@clavax.us>
2016-04-11 19:24:46 +0530

    identifiers-pep8

--- apps/vacationcard/urls.py
+++ apps/vacationcard/urls.py
@@ -1,14 +1,15 @@
-from django.conf.urls import url, include
 
+from django.conf.urls import url
 from . import views as vacationcard_views
-
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'api/vacationcard',vacationcard_views.VacationCardViewSet)
-router.register(r'api/businesscardvacation',vacationcard_views.BusinessCardVacationViewSet)
+router.register(r'api/vacationcard', vacationcard_views.VacationCardViewSet)
+router.register(r'api/businesscardvacation',
+                vacationcard_views.BusinessCardVacationViewSet)
 urlpatterns = router.urls
 
 urlpatterns += [
-	url(r'^vacationcard/merge$', vacationcard_views.VacationCardMerge.as_view(), name='vacationcard_merge')
+    url(r'^vacationcard/merge$',
+        vacationcard_views.VacationCardMerge.as_view(), name='vacationcard_merge')
 ]
