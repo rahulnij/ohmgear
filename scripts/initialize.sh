f71d4134cd3ebfefb7eac68a473c30c130987080
Rahul <rahuln@clavax.us>
2016-04-19 11:25:47 +0530

    addming pylint parseable file

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -7,6 +7,6 @@ WORKON_HOME=/var/www/python/envs
 . $WORKON_HOME/ohmgear/bin/activate
 
 pep8 apps/ ohmgear/ > pep8.log || true
-pylint apps/ ohmgear >  pylint.log || true
+pylint --rcfile=pylint.cfg apps/ ohmgear/ > pylint.log || true
 
 
