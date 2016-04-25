406e6d170fe366e0d8a8b4621303420cedc524ca
sazid <sazidk@clavax.us>
2016-04-25 10:59:12 +0530

    requst list api

--- apps/sendrequest/serializer.py
+++ apps/sendrequest/serializer.py
@@ -1,22 +1,55 @@
+import logging
+import json
 from rest_framework import serializers
 
 from models import SendRequest
-from apps.businesscards.models import BusinessCard
+from apps.contacts.models import Contacts
 # Serializers define the API representation.
+
+logger = logging.getLogger(__name__)
 
 
 class SendRequestSerializer(serializers.ModelSerializer):
-    
-    sender_data1 = serializers.SerializerMethodField('sender_data_func')
-    receiver_data1 = serializers.SerializerMethodField('receiver_data_func')
+
+    sender_data = serializers.SerializerMethodField('sender_data_func')
+    receiver_data = serializers.SerializerMethodField('receiver_data_func')
 
     def sender_data_func(self, obj):
-        #notes = BusinessCard.objects.get(contact_id=obj.contact_detail.id)
         data = {}
+        filter_type = self.context.get("filter_type")
+        if filter_type is not 'sent':
+            try:
+                get_contact_data = Contacts.objects.get(
+                    id=obj. sender_business_card_id.contact_detail.id)
+            except Contacts.DoesNotExist as e:
+                logger.error(
+                    "Object DoesNotExist: Contacts: {}, {}".format(
+                        obj.id, e))
+                return data
+        else:
+            return data
+
+        data['id'] = get_contact_data.id
+        data['bcard_json_data'] = get_contact_data.bcard_json_data
         return data
 
     def receiver_data_func(self, obj):
         data = {}
+        filter_type = self.context.get("filter_type")
+        if filter_type is not 'received':
+            try:
+                get_contact_data = Contacts.objects.get(
+                    id=obj.sender_business_card_id.contact_detail.id)
+            except Contacts.DoesNotExist as e:
+                logger.error(
+                    "Object DoesNotExist: Contacts: {}, {}".format(
+                        obj.id, e))
+                return data
+        else:
+            return data
+
+        data['id'] = get_contact_data.id
+        data['bcard_json_data'] = get_contact_data.bcard_json_data
         return data
 
     class Meta:
@@ -30,6 +63,6 @@ class SendRequestSerializer(serializers.ModelSerializer):
             'receiver_bcard_or_contact_id',
             'message',
             'request_status',
-            'sender_data1',
-            'receiver_data1',
+            'sender_data',
+            'receiver_data',
         )
