09dd303a761600009718dfca828d48c2a2f36f18
sazid <sazidk@clavax.us>
2016-05-02 12:27:26 +0530

    modification in aws

--- ohmgear/settings/aws.py
+++ ohmgear/settings/aws.py
@@ -3,7 +3,8 @@ AWS_CREDENTIAL = {
     "aws_secret_access_key": "NKofLzARVHh93gYgLpKb/jajf0NTFR6bQ7VXuJom",
     "region_name": "ap-southeast-1"}
 
-
+# apns : Apple Push Notification Service
+# gcm : Google Cloud Messaging
 AWS_PLATEFORM_APPLICATION_ARN = {
     "APNS": "arn:aws:sns:ap-southeast-1:625053715246:app/APNS_SANDBOX/KinbowApplePlateform",
     "GCM": ""}
