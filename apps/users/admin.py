ed1d2ded25a6dd89ebb2f0899c62858dd160e60c
dsharma <dsharma@clavax.us>
2016-05-02 18:49:24 +0530

    unregistered models

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,13 +1,20 @@
-from django.contrib import admin
-import csv
+
+# Django models
 from django.utils.encoding import smart_str
 from django.http import HttpResponse
-# from simple_history.admin import SimpleHistoryAdmin
-# Register your models here.
+from djcelery.models import (TaskState, WorkerState,
+                             PeriodicTask, IntervalSchedule, CrontabSchedule)
+from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import Token
+from django.contrib import admin
+admin.autodiscover()
+
+#Third Party models
+import csv
 
 from .models import (
-    User, 
-    UserType, 
+    User,
+    UserType,
     Profile,
     BusinessType,
     IncomeGroup,
@@ -48,7 +55,7 @@ def export_csv(modeladmin, request, queryset):
             # smart_str(obj.first_name),
             # smart_str(obj.last_name),
             smart_str(obj.email),
-            smart_str(obj.created_date)      
+            smart_str(obj.created_date)
         ])
     return response
 export_csv.short_description = u"Export selected objects to CSV"
@@ -56,26 +63,27 @@ export_csv.short_description = u"Export selected objects to CSV"
 
 class UserAdmin(admin.ModelAdmin):
     # code to display selected fields from the database
-    list_display = ('serial_number', 'email', 'created_date', 'get_status', 'user_type')
+    list_display = ('serial_number', 'email', 'created_date',
+                    'get_status', 'user_type')
     # search field criterion
     search_fields = ['email']
-    # custom drop down option in the admin 
+    # custom drop down option in the admin
     actions = [inactive_users, active_users, export_csv, ]
     # need this for bootstrap
     counter = 0
-    
+
     # non-editable fields in the admin
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
             return self.readonly_fields + ('password', 'user_type')
-        return self.readonly_fields  
-    
+        return self.readonly_fields
+
     def get_status(self, obj):
         if obj.status:
-            return 'Active'     
+            return 'Active'
         else:
             return 'InActive'
- 
+
     def serial_number(self, obj):
         self.counter = self.counter + 1
         return self.counter
@@ -84,7 +92,7 @@ class UserAdmin(admin.ModelAdmin):
     get_status.allow_tags = True
     list_per_page = 100
 
-    
+
 class UserTypeAdmin(admin.ModelAdmin):
     model = UserType
 
@@ -93,11 +101,12 @@ class ProfileAdmin(admin.ModelAdmin):
     model = Profile
     raw_id_fields = ('user',)
     search_fields = ('user__email',)
-        # code to display selected fields from the database
-    list_display = ('first_name', 'last_name','user_id_display','mobile_number','admin_thumbnail')
+    # code to display selected fields from the database
+    list_display = ('first_name', 'last_name', 'user_id_display',
+                    'mobile_number', 'admin_thumbnail')
 
     def user_id_display(self, obj):
-       return obj.user.email
+        return obj.user.email
     user_id_display.short_description = 'User Email'
 
 
@@ -127,6 +136,14 @@ class IncomeGroupAdmin(admin.ModelAdmin):
 class ConnectedAccountAdmin(admin.ModelAdmin):
     pass
 
+# Unregister Default Available Apps
+admin.site.unregister(Group)
+admin.site.unregister(PeriodicTask)
+admin.site.unregister(CrontabSchedule)
+admin.site.unregister(TaskState)
+admin.site.unregister(WorkerState)
+admin.site.unregister(IntervalSchedule)
+admin.site.unregister(Token)
 
 admin.site.register(User, UserAdmin)
 admin.site.register(UserType, UserTypeAdmin)
